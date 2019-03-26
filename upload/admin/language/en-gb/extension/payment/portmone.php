4de80947f24afeb4fe82d1609bfa824391527ecd
zhuiks <zhuke@ukr.net>
2019-03-26 09:06:21 +0200

    initial commit

---
+++ upload/admin/language/en-gb/extension/payment/portmone.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']      = 'Portmone Credit/Debit cards';
+
+// Text 
+$_['text_payment']       = 'Payment'; 
+$_['text_success']       = 'Success: You have modified Portmone account details!';
+$_['text_portmone']       = '<a href="https://portmone.com.ua/r3/uk/ecommerce/auth/" target="_blank">portmone.com</a>';
+$_['text_authenticate']  = 'Authenticate';
+
+// Entry
+$_['entry_payee_id']     = 'Company (payee) ID:';
+$_['entry_transaction']  = 'Transaction Method:';
+$_['entry_total']        = 'Total:<br /><span class="help">The checkout total the order must reach before this payment method becomes active.</span>';
+$_['entry_order_status'] = 'Order Status:';
+$_['entry_geo_zone']     = 'Geo Zone:';
+$_['entry_status']       = 'Status:';
+$_['entry_sort_order']   = 'Sort Order:';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify payment Portmone!';
+$_['error_payee_id']     = 'Company ID Required!';
+?>
